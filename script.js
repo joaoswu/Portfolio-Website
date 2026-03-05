77ac81d8a88f99a5ae91eba2b580463b97301edd
joaoswu <joaofilmss33@gmail.com>
2026-03-05 14:56:03 +0000

    feat: update MY_MOOD constant to Happy

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // ==========================================
     // CUSTOMIZATION SECTION
     // ==========================================
-    const MY_MOOD = "Regret. Guilt. Disappointment.";
+    const MY_MOOD = "Happy";
     const TIKTOK_FOLLOWERS_COUNT = 47659; // Type your numbers here
     const TIKTOK_LIKES_COUNT = 7700000;
     const TIKTOK_VIDEOS_COUNT = 369;
